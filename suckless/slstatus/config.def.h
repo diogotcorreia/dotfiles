c2b3f2e861859e870cfdb9afda7bbe5a6b7da1b4
Diogo Correia <me@diogotc.com>
2021-04-21 13:24:49 +0100

    Add network speed, cpu and ram usage to slstatus

--- suckless/slstatus/config.def.h
+++ suckless/slstatus/config.def.h
@@ -65,5 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+  { netspeed_rx, "%s ↓ ",     "enp8s0"},
+  { netspeed_tx, "%s ↑ ",     "enp8s0"},
+  { cpu_perc, "CPU %s%% ",    NULL    },
+  { ram_perc, "RAM %s%% ",    NULL    },
 	{ datetime, "%s",           "%F %T" },
 };
