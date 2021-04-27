d5c68315512add7e9c215d2618876b8480e6edce
Diogo Correia <me@diogotc.com>
2021-04-27 22:16:19 +0100

    Change DWM lockscreen keybind

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -105,7 +105,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = -1 } },
-	{ MODKEY,                       XK_l,      spawn,          {.v = lockcmd } },
+	{ MODKEY,                       XK_o,      spawn,          {.v = lockcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
