fb2b594273b837c7d54922460dfc4f56990ad351
Diogo Correia <me@diogotc.com>
2021-04-27 15:35:04 +0100

    Fix wrong floating value on flameshot rule DWM

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "flameshot",NULL,       NULL,       0,            2,           -1 },
+	{ "flameshot",NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
