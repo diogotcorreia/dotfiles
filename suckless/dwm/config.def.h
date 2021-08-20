568f6c43726cc535e2c07856d5dbc30e8b2b393e
Diogo Correia <me@diogotc.com>
2021-08-20 09:42:21 +0100

    Migrate to Nord color theme

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -13,11 +13,11 @@ static const char *fonts[]          = { "Fira Code:size=10",
                                         "monospace:size=10"
                                       };
 static const char dmenufont[]       = "MesloLGS NF:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2E3440";
+static const char col_gray2[]       = "#434C5E";
+static const char col_gray3[]       = "#D8DEE9";
+static const char col_gray4[]       = "#ECEFF4";
+static const char col_cyan[]        = "#5E81AC";
 static const unsigned int baralpha = 0x96;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
