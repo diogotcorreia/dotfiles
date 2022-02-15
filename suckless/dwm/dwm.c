59a776959cd419751e752592fbb3fd153fcf39a5
Diogo Correia <me@diogotc.com>
2022-02-15 23:31:58 +0000

    Add dwm-focusmonmouse patch

--- suckless/dwm/dwm.c
+++ suckless/dwm/dwm.c
@@ -867,6 +867,7 @@ focusmon(const Arg *arg)
 	if ((m = dirtomon(arg->i)) == selmon)
 		return;
 	unfocus(selmon->sel, 0);
+	XWarpPointer(dpy, None, m->barwin, 0, 0, 0, 0, m->mw / 2, m->mh / 2);
 	selmon = m;
 	focus(NULL);
 }
