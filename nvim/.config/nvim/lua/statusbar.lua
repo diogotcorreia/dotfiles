3ff6d4033daad0038c04b988a8e7e45bd66e5a47
Diogo Correia <me@diogotc.com>
2023-01-21 11:49:27 +0000

    Show word count on nvim status bar

--- nvim/.config/nvim/lua/statusbar.lua
+++ nvim/.config/nvim/lua/statusbar.lua
@@ -1,5 +1,17 @@
 -- Customize statusbar (with lualine.nvim)
 
+local function getWordCount()
+  local word_count = vim.fn.wordcount().visual_words
+  if word_count == nul then
+    word_count = vim.fn.wordcount().words
+  end
+
+  if word_count == 1 then
+    return tostring(word_count) .. " word"
+  end
+  return tostring(word_count) .. " words"
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -14,7 +26,7 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype', 'filesize'},
+    lualine_x = {getWordCount, 'encoding', 'fileformat', 'filetype', 'filesize'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
