90b24c1a690e0f1f565c30786e21058a8c1c65d0
Diogo Correia <me@diogotc.com>
2023-01-27 02:14:03 +0000

    Update bacchus password

--- hosts/bacchus/system.nix
+++ hosts/bacchus/system.nix
@@ -57,7 +57,7 @@
     mutableUsers = false;
     users = {
       ${user} = {
-        initialPassword = "test";
+        hashedPassword = "$y$j9T$U.2Gk7rztC3F8cSSBzElT/$6IJUtc3etUKuO8tWY4mCmQZ6LaRsTuteKPcXxJKnsZC";
         openssh.authorizedKeys.keys = sshKeys;
       };
     };
