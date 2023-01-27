77ee82c6fac6740ff9c4955824a279f3a9230d38
Diogo Correia <me@diogotc.com>
2023-01-27 01:59:24 +0000

    Fix dwmblocks crashing with long statusline

--- overlays/dwmblocks/default.nix
+++ overlays/dwmblocks/default.nix
@@ -14,6 +14,15 @@ final: prev: rec {
       rev = "5a6fa8d550c11552480f10e660073ca294d446b1";
       sha256 = "00lxfxsrvhm60zzqlcwdv7xkqzya69mgpi2mr3ivzbc8s9h8nwqx";
     };
-    patches = [ ./0001-cleanup.diff ./0002-max-length.diff ./0003-config.diff ];
+    patches = [
+      (prev.fetchpatch {
+        url =
+          "https://github.com/torrinfail/dwmblocks/commit/4d92b6ca6caae215a79f00d88404c018b9eac15f.patch";
+        sha256 = "sha256-Rahx1EU9ijsKuqjdcuODDLWhsAq4X23j1kwstGXJrVQ=";
+      })
+      ./0001-cleanup.diff
+      ./0002-max-length.diff
+      ./0003-config.diff
+    ];
   });
 }
