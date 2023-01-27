8a8e98811eff52c6c9d68f37b4e8c227b17845a3
Diogo Correia <me@diogotc.com>
2023-01-27 22:40:22 +0000

    Re-enable personal neovim config

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -9,7 +9,7 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.neovim;
-  # personal = config.modules.personal.enable;
+  personal = config.modules.personal.enable;
   git = config.modules.shell.git.enable;
   commonGrammars = with pkgs.unstable.tree-sitter-grammars; [
     tree-sitter-bash
@@ -19,21 +19,22 @@ let
     tree-sitter-nix
     tree-sitter-python
   ];
-  # TODO
-  personalGrammars = [ ];
-  # personalGrammars = if personal then with pkgs.unstable.tree-sitter-grammars; [
-  #   tree-sitter-c
-  #   tree-sitter-cpp
-  #   tree-sitter-java
-  #   tree-sitter-javascript
-  #   tree-sitter-latex
-  #   tree-sitter-lua
-  #   tree-sitter-rust
-  #   tree-sitter-toml
-  #   tree-sitter-typescript
-  #   tree-sitter-vim
-  #   tree-sitter-yaml
-  # ] else [];
+  personalGrammars = if personal then
+    with pkgs.unstable.tree-sitter-grammars; [
+      tree-sitter-c
+      tree-sitter-cpp
+      tree-sitter-java
+      tree-sitter-javascript
+      tree-sitter-latex
+      tree-sitter-lua
+      tree-sitter-rust
+      tree-sitter-toml
+      tree-sitter-typescript
+      tree-sitter-vim
+      tree-sitter-yaml
+    ]
+  else
+    [ ];
   commonPlugins = with pkgs.unstable.vimPlugins; [
     nvim-web-devicons
     {
@@ -187,114 +188,115 @@ let
       '';
     }
   ];
-  # TODO
-  personalPlugins = [ ];
-  #   personalPlugins = if personal then with pkgs.unstable.vimPlugins; [
-  #     {
-  #       plugin = nvim-lspconfig;
-  #       type = "lua";
-  #       config = ''
-  # local lsp = require'lspconfig'
-  #
-  # local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  # capabilities.textDocument.completion.completionItem.snippetSupport = true
-  # capabilities.textDocument.completion.completionItem.resolveSupport = {
-  # 	properties = {
-  # 		'documentation',
-  # 		'detail',
-  # 		'additionalTextEdits',
-  # 	}
-  # }
-  #
-  # lsp.rust_analyzer.setup{
-  # 	capabilities = capabilities,
-  # 	on_attach = require'generic_lsp'
-  # }
-  # lsp.texlab.setup{
-  # 	capabilities = capabilities,
-  # 	on_attach = require'generic_lsp'
-  # }
-  #       '';
-  #     }
-  #     lsp_extensions-nvim
-  #
-  #     {
-  #       plugin = presence-nvim;
-  #       config = ''
-  #       let g:presence_auto_update       = 1
-  #       let g:presence_editing_text      = "Editing %s"
-  #       let g:presence_workspace_text    = "Working on %s"
-  #       let g:presence_neovim_image_text = "vim but better"
-  #       let g:presence_main_image        = "neovim"
-  #       '';
-  #     }
-  #
-  #     luasnip
-  #     {
-  #       plugin = nvim-cmp;
-  #       type = "lua";
-  #       config = ''
-  # -- Setup nvim-cmp.
-  # local cmp = require'cmp'
-  #
-  # cmp.setup({
-  #   snippet = {
-  #     -- REQUIRED - you must specify a snippet engine
-  #     expand = function(args)
-  #       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-  #     end,
-  #   },
-  #   window = {
-  #     -- completion = cmp.config.window.bordered(),
-  #     -- documentation = cmp.config.window.bordered(),
-  #   },
-  #   mapping = cmp.mapping.preset.insert({
-  #     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  #     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  #     ['<C-Space>'] = cmp.mapping.complete(),
-  #     ['<C-e>'] = cmp.mapping.abort(),
-  #     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  #   }),
-  #   sources = cmp.config.sources({
-  #     { name = 'nvim_lsp' },
-  #     { name = 'luasnip' }, -- For luasnip users.
-  #   }, {
-  #     { name = 'buffer' },
-  #   })
-  # })
-  #
-  # -- Set configuration for specific filetype.
-  # cmp.setup.filetype('gitcommit', {
-  #   sources = cmp.config.sources({
-  #     -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  #   }, {
-  #     { name = 'buffer' },
-  #   })
-  # })
-  #
-  # -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-  # cmp.setup.cmdline('/', {
-  #   mapping = cmp.mapping.preset.cmdline(),
-  #   sources = {
-  #     { name = 'buffer' }
-  #   }
-  # })
-  #
-  # -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  # cmp.setup.cmdline(':', {
-  #   mapping = cmp.mapping.preset.cmdline(),
-  #   sources = cmp.config.sources({
-  #     { name = 'path' }
-  #   }, {
-  #     { name = 'cmdline' }
-  #   })
-  # })
-  #       '';
-  #     }
-  #     cmp_luasnip
-  #     cmp-treesitter
-  #     cmp-nvim-lsp
-  #   ] else [];
+  personalPlugins = if personal then
+    with pkgs.unstable.vimPlugins; [
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = ''
+          local lsp = require'lspconfig'
+
+          local capabilities = require('cmp_nvim_lsp').default_capabilities()
+          capabilities.textDocument.completion.completionItem.snippetSupport = true
+          capabilities.textDocument.completion.completionItem.resolveSupport = {
+            properties = {
+              'documentation',
+              'detail',
+              'additionalTextEdits',
+            }
+          }
+
+          lsp.rust_analyzer.setup{
+            capabilities = capabilities,
+            on_attach = require'generic_lsp'
+          }
+          lsp.texlab.setup{
+            capabilities = capabilities,
+            on_attach = require'generic_lsp'
+          }
+        '';
+      }
+      lsp_extensions-nvim
+
+      {
+        plugin = presence-nvim;
+        config = ''
+          let g:presence_auto_update       = 1
+          let g:presence_editing_text      = "Editing %s"
+          let g:presence_workspace_text    = "Working on %s"
+          let g:presence_neovim_image_text = "vim but better"
+          let g:presence_main_image        = "neovim"
+        '';
+      }
+
+      luasnip
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = ''
+          -- Setup nvim-cmp.
+          local cmp = require'cmp'
+
+          cmp.setup({
+            snippet = {
+              -- REQUIRED - you must specify a snippet engine
+              expand = function(args)
+                require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+              end,
+            },
+            window = {
+              -- completion = cmp.config.window.bordered(),
+              -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<C-e>'] = cmp.mapping.abort(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'luasnip' }, -- For luasnip users.
+            }, {
+              { name = 'buffer' },
+            })
+          })
+
+          -- Set configuration for specific filetype.
+          cmp.setup.filetype('gitcommit', {
+            sources = cmp.config.sources({
+              -- { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+            }, {
+              { name = 'buffer' },
+            })
+          })
+
+          -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+          cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+              { name = 'buffer' }
+            }
+          })
+
+          -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+          cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+              { name = 'path' }
+            }, {
+              { name = 'cmdline' }
+            })
+          })
+        '';
+      }
+      cmp_luasnip
+      cmp-treesitter
+      cmp-nvim-lsp
+    ]
+  else
+    [ ];
   twoSpaceIndentConfig = ''
     setlocal shiftwidth=2
     setlocal softtabstop=2
