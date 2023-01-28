adcd80cb8d56f34b6fbe535a1d58db3eaf904f12
Diogo Correia <me@diogotc.com>
2023-01-28 15:50:31 +0000

    Add NVIDIA drivers to bacchus

--- hosts/bacchus/system.nix
+++ hosts/bacchus/system.nix
@@ -52,10 +52,33 @@
     pulse.enable = true;
   };
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+  };
+  services.xserver.screenSection = ''
+    Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+    Option         "AllowIndirectGLXProtocol" "off"
+    Option         "TripleBuffer" "on"
+  '';
+
   modules.dnsovertls.enable = true;
   modules.dwm.enable = true;
   modules.ist.enable = true;
   modules.syncthing.enable = true;
+
+  environment.systemPackages = let
+    nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '';
+  in ([ nvidia-offload ]);
 
   users = {
     mutableUsers = false;
