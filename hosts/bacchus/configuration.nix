fcb94e737cef2a19398ff011ad4ba5b622e51862
Diogo Correia <me@diogotc.com>
2023-02-11 18:52:15 +0000

    Enable OpenGL and use nvidia.forceFullCompositionPipeline

--- hosts/bacchus/configuration.nix
+++ hosts/bacchus/configuration.nix
@@ -66,17 +66,14 @@
   # Nvidia
   # TODO move to module
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
   hardware.nvidia.prime = {
     offload.enable = true;
 
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:2:0:0";
   };
-  services.xserver.screenSection = ''
-    Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-    Option         "AllowIndirectGLXProtocol" "off"
-    Option         "TripleBuffer" "on"
-  '';
+  hardware.nvidia.forceFullCompositionPipeline = true;
   environment.systemPackages = let
     nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
       export __NV_PRIME_RENDER_OFFLOAD=1
