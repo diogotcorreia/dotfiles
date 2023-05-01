6ce8bcedf732f63fa1123994dee9d2e912aad128
Diogo Correia <me@diogotc.com>
2023-05-01 21:26:04 +0100

    feat: turn off monitor after locking with slock

---
+++ overlays/slock/default.nix
@@ -0,0 +1,18 @@
+# overlays/slock/default.nix
+#
+# Author: Diogo Correia <me@diogotc.com>
+# URL:    https://github.com/diogotcorreia/dotfiles
+#
+# Apply patches to slock
+
+{ ... }:
+final: prev: rec {
+  slock = prev.slock.overrideAttrs (oldAttrs: rec {
+    patches = [
+      (prev.fetchpatch {
+        url = "https://tools.suckless.org/slock/patches/dpms/slock-dpms-1.4.diff";
+        sha256 = "sha256-hfe71OTpDbqOKhu/LY8gDMX6/c07B4sZ+mSLsbG6qtg=";
+      })
+    ];
+  });
+}
