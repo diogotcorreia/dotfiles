62bf659bea16d2ddfc9f9ba43f37e33d8e9aa676
Diogo Correia <me@diogotc.com>
2023-05-01 21:17:20 +0100

    feat: add fuzzy match patch to dmenu

---
+++ overlays/dmenu/default.nix
@@ -0,0 +1,18 @@
+# overlays/dmenu/default.nix
+#
+# Author: Diogo Correia <me@diogotc.com>
+# URL:    https://github.com/diogotcorreia/dotfiles
+#
+# Apply patches to dmenu
+
+{ ... }:
+final: prev: rec {
+  dmenu = prev.dmenu.overrideAttrs (oldAttrs: rec {
+    patches = [
+      (prev.fetchpatch {
+        url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
+        sha256 = "sha256-zfmsKfN791z6pyv+gA6trdfKvNnCCULazVtk1sibDgA=";
+      })
+    ];
+  });
+}
