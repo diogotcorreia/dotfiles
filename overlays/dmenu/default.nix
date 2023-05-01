4cb971700e6f5045b3aa1f349b66b1117e22bf62
Diogo Correia <me@diogotc.com>
2023-05-01 21:17:44 +0100

    feat: match dmenu bar height with dwm's

--- overlays/dmenu/default.nix
+++ overlays/dmenu/default.nix
@@ -13,6 +13,8 @@ final: prev: rec {
         url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
         sha256 = "sha256-zfmsKfN791z6pyv+gA6trdfKvNnCCULazVtk1sibDgA=";
       })
+      # Based on https://tools.suckless.org/dmenu/patches/bar_height/
+      ./0001-dmenu-bar-height.diff
     ];
   });
 }
