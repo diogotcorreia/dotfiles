be2cba42c257d7d682e0c8c864b234928f87ef18
Diogo Correia <me@diogotc.com>
2023-05-04 16:40:58 +0100

    style: run formatter

--- overlays/dmenu/default.nix
+++ overlays/dmenu/default.nix
@@ -10,7 +10,8 @@ final: prev: rec {
   dmenu = prev.dmenu.overrideAttrs (oldAttrs: rec {
     patches = [
       (prev.fetchpatch {
-        url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
+        url =
+          "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
         sha256 = "sha256-zfmsKfN791z6pyv+gA6trdfKvNnCCULazVtk1sibDgA=";
       })
       # Based on https://tools.suckless.org/dmenu/patches/bar_height/
--- overlays/slock/default.nix
+++ overlays/slock/default.nix
@@ -10,7 +10,8 @@ final: prev: rec {
   slock = prev.slock.overrideAttrs (oldAttrs: rec {
     patches = [
       (prev.fetchpatch {
-        url = "https://tools.suckless.org/slock/patches/dpms/slock-dpms-1.4.diff";
+        url =
+          "https://tools.suckless.org/slock/patches/dpms/slock-dpms-1.4.diff";
         sha256 = "sha256-hfe71OTpDbqOKhu/LY8gDMX6/c07B4sZ+mSLsbG6qtg=";
       })
     ];
