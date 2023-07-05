41c00e2b6604de7a7b11ea1e04c360068b55e41a
Diogo Correia <me@diogotc.com>
2023-07-05 18:25:53 +0100

    feat: add system diff with nvd on nixos rebuild

--- modules/personal.nix
+++ modules/personal.nix
@@ -102,5 +102,9 @@ in {
         };
       };
     };
+
+    system.activationScripts.diff = ''
+      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+    '';
   };
 }
