9647acf35c66edc200b0e88319c78c0e31b09390
Diogo Correia <me@diogotc.com>
2023-10-11 12:25:37 +0200

    feat: enable hardware acceleration on hera for jellyfin

--- hosts/hera/jellyfin.nix
+++ hosts/hera/jellyfin.nix
@@ -24,7 +24,19 @@ let
   mediaGroup = "diskstation-media";
 in {
 
-  # TODO setup hardware acceleration
+  # https://nixos.wiki/wiki/Accelerated_Video_Playback
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
   services.jellyfin.enable = true;
   services.radarr = {
