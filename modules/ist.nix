ce9c60b31e95a9349de4cbf79011af2e025cede3
Diogo Correia <me@diogotc.com>
2023-12-28 02:06:41 +0000

    feat(ist): remove course url shortcuts

--- modules/ist.nix
+++ modules/ist.nix
@@ -16,17 +16,6 @@ let
       "https://suporte.dsi.tecnico.ulisboa.pt/sites/default/files/files/tecnico.ovpn";
     sha256 = "sha256-3vQ5eyrB2IEKHJXXDJk3kPXRbNwGsRpcN8hmPl7ihBQ=";
   };
-
-  courseUrls = {
-    cg =
-      "https://fenix.tecnico.ulisboa.pt/disciplinas/CGra2/2022-2023/2-semestre";
-    comp =
-      "https://fenix.tecnico.ulisboa.pt/disciplinas/Com/2022-2023/2-semestre";
-    es =
-      "https://fenix.tecnico.ulisboa.pt/disciplinas/ESof2/2022-2023/2-semestre";
-    sd =
-      "https://fenix.tecnico.ulisboa.pt/disciplinas/SDis2/2022-2023/2-semestre";
-  };
 in {
   options.modules.ist.enable = mkEnableOption "ist";
 
@@ -60,14 +49,5 @@ in {
         auth-user-pass ${config.age.secrets.openvpnIstAuthUserPass.path}
       '';
     };
-
-    # Course shortcuts
-    hm.home.packages = map (courseName:
-      pkgs.writeScriptBin courseName ''
-        ${pkgs.xdg-utils}/bin/xdg-open ${
-          escapeShellArg (getAttr courseName courseUrls)
-        }
-      '') (attrNames courseUrls);
-
   };
 }
