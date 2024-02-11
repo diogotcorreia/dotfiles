9bbbffc9a93ec7bec4370d95f6a6cda6c5f357c5
Diogo Correia <me@diogotc.com>
2024-02-11 01:35:43 +0100

    refactor(dnsoverhttps): replace dnscrypt-proxy with dnsproxy

--- modules/services/dnsoverhttps.nix
+++ modules/services/dnsoverhttps.nix
@@ -7,10 +7,18 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf mkOption types;
   cfg = config.modules.services.dnsoverhttps;
+
+  upstreams = [
+    "sdns://AgcAAAAAAAAABzEuMS4xLjEAEmRucy5jbG91ZGZsYXJlLmNvbQovZG5zLXF1ZXJ5" # 1.1.1.1
+    "sdns://AgcAAAAAAAAABzEuMC4wLjEAEmRucy5jbG91ZGZsYXJlLmNvbQovZG5zLXF1ZXJ5" # 1.0.0.1
+    "sdns://AgcAAAAAAAAAFlsyNjA2OjQ3MDA6NDcwMDo6MTExMV0AIDFkb3QxZG90MWRvdDEuY2xvdWRmbGFyZS1kbnMuY29tCi9kbnMtcXVlcnk" # [2606:4700:4700::1111]
+    "sdns://AgcAAAAAAAAAFlsyNjA2OjQ3MDA6NDcwMDo6MTAwMV0AIDFkb3QxZG90MWRvdDEuY2xvdWRmbGFyZS1kbnMuY29tCi9kbnMtcXVlcnk" # [2606:4700:4700::1001]
+  ];
 in {
   options.modules.services.dnsoverhttps = {
     enable = mkEnableOption "Cloudflare DNS over HTTPS proxy";
@@ -31,27 +39,67 @@ in {
       dhcpcd.extraConfig = "nohook resolv.conf";
     };
 
-    services.dnscrypt-proxy2 = {
-      enable = true;
-      settings = {
-        listen_addresses = [
-          (
-            if cfg.all-interfaces
-            then "0.0.0.0:53"
-            else "127.0.0.53:53"
-          )
+    systemd.services.dnsproxy = let
+      extraArgs = [
+        "--cache"
+        "--listen=${
+          if cfg.all-interfaces
+          then "0.0.0.0"
+          else "127.0.0.53"
+        }"
+      ];
+
+      finalArgs = map (upstream: "--upstream=${upstream}") upstreams ++ extraArgs;
+    in {
+      description = "dnsproxy client";
+      wants = [
+        "network-online.target"
+        "nss-lookup.target"
+      ];
+      before = [
+        "nss-lookup.target"
+      ];
+      wantedBy = [
+        "multi-user.target"
+      ];
+      serviceConfig = {
+        AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+        CacheDirectory = "dnsproxy";
+        DynamicUser = true;
+        ExecStart = "${lib.getExe pkgs.dnsproxy} ${lib.escapeShellArgs finalArgs}";
+        LockPersonality = true;
+        LogsDirectory = "dnsproxy";
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        NonBlocking = true;
+        PrivateDevices = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        Restart = "always";
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
         ];
-
-        sources.public-resolvers = {
-          urls = [
-            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-          ];
-          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-        };
-
-        server_names = ["cloudflare" "cloudflare-ipv6"];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RuntimeDirectory = "dnsproxy";
+        StateDirectory = "dnsproxy";
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "@chown"
+          "~@aio"
+          "~@keyring"
+          "~@memlock"
+          "~@setuid"
+          "~@timer"
+        ];
       };
     };
   };
