431e8bc14fec0181ae24e936e5b737da468ae5d7
Diogo Correia <me@diogotc.com>
2024-02-26 20:12:19 +0100

    fix(cfdyndns): crash due to CAA records in the zone
    See https://github.com/nrdxp/cfdyndns/issues/54
    See https://github.com/nrdxp/cfdyndns/pull/56

--- overlays/cfdyndns.nix
+++ overlays/cfdyndns.nix
@@ -5,5 +5,14 @@
 #
 # Use cfdyndns from nixos-unstable
 {...}: final: prev: rec {
-  cfdyndns = prev.unstable.cfdyndns;
+  cfdyndns = prev.unstable.cfdyndns.overrideAttrs (oldAttrs: rec {
+    patches = [
+      # Fix cfdyndns not working if CAA records are present in the zone
+      # https://github.com/nrdxp/cfdyndns/pull/56
+      (prev.fetchpatch {
+        url = "https://github.com/nrdxp/cfdyndns/commit/1c7a3a4a2f0426dddce8e7195360aa39219b27f0.patch";
+        hash = "sha256-aUpPUzOcfFQjf7GFfAlM2LEZiSEQULoKNXLt+5SnpAE=";
+      })
+    ];
+  });
 }
