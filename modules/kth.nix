e5dbe296234ddb5dcfc0fa0d28192cb7cfb3c1d5
Diogo Correia <me@diogotc.com>
2024-03-18 11:00:10 +0100

    feat(kth): add fcrypto and langsec course shortcuts

--- modules/kth.nix
+++ modules/kth.nix
@@ -18,7 +18,9 @@
     acrypto = "https://canvas.kth.se/courses/45246";
     bnss = "https://canvas.kth.se/courses/44880";
     cybsam = "https://canvas.kth.se/courses/41786";
+    fcrypto = "https://canvas.kth.se/courses/46237";
     hwsec = "https://canvas.kth.se/courses/45192";
+    langsec = "https://canvas.kth.se/courses/46200";
   };
 in {
   options.modules.kth.enable = mkEnableOption "kth";
