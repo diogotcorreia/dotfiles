6392b6c5bf1d9aaf11d171e66e03219fc3fbbd14
Diogo Correia <me@diogotc.com>
2024-06-19 21:25:39 +0100

    chore: update discord-nordic theme to v4.10.13

--- modules/graphical/programs.nix
+++ modules/graphical/programs.nix
@@ -9,8 +9,8 @@
   cfg = config.modules.graphical.programs;
 
   discordThemeFile = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/orblazer/discord-nordic/v4.10.9/uniform/nordic.theme.css";
-    hash = "sha256-SDLprtPs4F3NirCHn/SL7WAWl8iK0+4JglVr6oP0ejs=";
+    url = "https://raw.githubusercontent.com/orblazer/discord-nordic/v4.10.13/uniform/nordic.theme.css";
+    hash = "sha256-qaDvDIH3vyYKxbGgjuXkabGxpCr6qTeloQTinl7a62A=";
   };
 in {
   options.modules.graphical.programs.enable = mkEnableOption "programs";
