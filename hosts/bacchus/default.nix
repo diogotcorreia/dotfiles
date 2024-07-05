diogotcorreia/dotfiles

commit c36bd9fad142ea014cd5de0618ba052233031f79

refactor: move zfs impermanence partition setup to profile
This greatly removes duplicate code across all the hosts that have the
exact same partition layout.

hosts/bacchus/default.nix
@@ -191,8 +191,6 @@
       zsh.enable = true;
     };
     impermanence = {
-      enable = true;
-      # Impermanence (root on tmpfs)
       directories = [
         "/etc/NetworkManager/system-connections"
         "/var/lib/bluetooth"

hosts/apollo/default.nix
@@ -9,6 +9,7 @@
   imports = with profiles; [
     graphical.firefox
     graphical.thunderbird
+    hardware.filesystem.zfs-impermanence
     hardware.zram
     misc.cybersec
     misc.kth
@@ -17,9 +18,9 @@
     virtualisation.docker
   ];
 
-  # ZFS configuration
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
+  networking.hostId = "30c1f688";
+
+  my.filesystem.mainDisk = "/dev/nvme0n1";
 
   # /tmp configuration
   boot.tmp.cleanOnBoot = true;
@@ -185,8 +186,6 @@
       zsh.enable = true;
     };
     impermanence = {
-      enable = true;
-      # Impermanence (root on tmpfs)
       directories = [
         "/etc/NetworkManager/system-connections"
         "/var/lib/bluetooth"

hosts/bro/default.nix
@@ -7,6 +7,7 @@
   ...
 }: {
   imports = with profiles; [
+    hardware.filesystem.zfs-impermanence
     hardware.zram
     networking.ddns.cloudflare
     security.fail2ban
@@ -16,9 +17,9 @@
     services.ssh
   ];
 
-  # ZFS configuration
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
+  networking.hostId = "29e7efc9";
+
+  my.filesystem.mainDisk = "/dev/sda";
 
   # /tmp configuration
   boot.tmp.cleanOnBoot = true;
@@ -126,8 +127,6 @@
       zsh.enable = true;
     };
     impermanence = {
-      enable = true;
-      # Impermanence (root on tmpfs)
       directories = ["/var/lib/acme"];
     };
   };

hosts/feb/default.nix
@@ -6,6 +6,7 @@
   ...
 }: {
   imports = with profiles; [
+    hardware.filesystem.zfs-impermanence
     hardware.zram
     networking.ddns.cloudflare
     security.fail2ban
@@ -13,9 +14,8 @@
     services.ssh
   ];
 
-  # ZFS configuration
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
+  networking.hostId = "1215a7f5";
+  my.filesystem.mainDisk = "/dev/nvme0n1";
 
   # /tmp configuration
   boot.tmp.cleanOnBoot = true;
@@ -119,8 +119,6 @@
       zsh.enable = true;
     };
     impermanence = {
-      enable = true;
-      # Impermanence (root on tmpfs)
       directories = ["/var/lib/acme"];
     };
   };

hosts/hera/default.nix
@@ -7,6 +7,7 @@
   ...
 }: {
   imports = with profiles; [
+    hardware.filesystem.zfs-impermanence
     hardware.zram
     security.fail2ban
     services.caddy.common
@@ -14,9 +15,10 @@
     virtualisation.docker
   ];
 
-  # ZFS configuration
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
+  # Host Id
+  networking.hostId = "9832f4c7";
+
+  my.filesystem.mainDisk = "/dev/sda";
 
   # /tmp configuration
   boot.tmp.cleanOnBoot = true;
@@ -158,8 +160,6 @@
       zsh.enable = true;
     };
     impermanence = {
-      enable = true;
-      # Impermanence (root on tmpfs)
       directories = [
         "/etc/NetworkManager/system-connections"
         "/var/lib/acme"

flake.nix
@@ -89,7 +89,6 @@
         inputs.impermanence.nixosModules.impermanence
         inputs.agenix.nixosModules.default
         inputs.lanzaboote.nixosModules.lanzaboote
-        inputs.disko.nixosModules.disko
         inputs.attic.nixosModules.atticd
       ];
     };

hosts/apollo/hardware.nix
@@ -4,22 +4,13 @@
   lib,
   modulesPath,
   ...
-}: let
-  # a host id can be generated with `head -c4 /dev/urandom | od -A none -t x4`
-  hostId = "30c1f688";
-
-  diskDevice = "/dev/nvme0n1";
-  espSize = "512M";
-in {
+}: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
-
-  boot.supportedFilesystems = ["zfs"];
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -29,101 +20,9 @@ in {
   hardware.cpu.amd.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # Partitions configuration (using disko)
-  disko.devices = {
-    disk = {
-      disk0 = {
-        type = "disk";
-        device = diskDevice;
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = espSize;
-              type = "EF00"; # for EFI boot
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            rpool = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "rpool";
-              };
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      rpool = {
-        type = "zpool";
-        options = {ashift = "12";};
-        rootFsOptions = {
-          acltype = "posixacl";
-          atime = "off";
-          canmount = "off";
-          compression = "zstd";
-          dnodesize = "auto";
-          normalization = "formD";
-          xattr = "sa";
-          mountpoint = "none";
-        };
-
-        datasets = {
-          "local/root" = {
-            type = "zfs_fs";
-            mountpoint = "/";
-            mountOptions = ["zfsutil"];
-
-            postCreateHook = ''
-              zfs snapshot rpool/local/root@blank
-            '';
-          };
-          "local/nix" = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            mountOptions = ["zfsutil"];
-          };
-          # To be used in emergencies if the disk goes full
-          "local/reserved" = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "none";
-              refreservation = "1G";
-            };
-          };
-          "safe/persist" = {
-            type = "zfs_fs";
-            mountpoint = "/persist";
-            mountOptions = ["zfsutil"];
-          };
-          "safe/home" = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            mountOptions = ["zfsutil"];
-          };
-        };
-      };
-    };
-  };
-
-  # sadly disko can't handle this for us
-  fileSystems."/persist".neededForBoot = true;
-  fileSystems."/home".neededForBoot = true;
-
   # External Drive
   fileSystems."/media/files" = {
     device = "/dev/disk/by-uuid/7257ad05-1068-4545-936b-94231db82bb1";
     fsType = "ext4";
   };
-
-  # Persistence (root dataset is rollback'ed to a blank snapshot)
-  networking.hostId = hostId;
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    zfs rollback -r rpool/local/root@blank
-  '';
 }

hosts/bro/hardware.nix
@@ -4,22 +4,13 @@
   lib,
   modulesPath,
   ...
-}: let
-  # a host id can be generated with `head -c4 /dev/urandom | od -A none -t x4`
-  hostId = "29e7efc9";
-
-  diskDevice = "/dev/sda";
-  espSize = "512M";
-in {
+}: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
-
-  boot.supportedFilesystems = ["zfs"];
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -29,96 +20,4 @@ in {
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  # Partitions configuration (using disko)
-  disko.devices = {
-    disk = {
-      disk0 = {
-        type = "disk";
-        device = diskDevice;
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = espSize;
-              type = "EF00"; # for EFI boot
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            rpool = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "rpool";
-              };
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      rpool = {
-        type = "zpool";
-        options = {ashift = "12";};
-        rootFsOptions = {
-          acltype = "posixacl";
-          atime = "off";
-          canmount = "off";
-          compression = "zstd";
-          dnodesize = "auto";
-          normalization = "formD";
-          xattr = "sa";
-          mountpoint = "none";
-        };
-
-        datasets = {
-          "local/root" = {
-            type = "zfs_fs";
-            mountpoint = "/";
-            mountOptions = ["zfsutil"];
-
-            postCreateHook = ''
-              zfs snapshot rpool/local/root@blank
-            '';
-          };
-          "local/nix" = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            mountOptions = ["zfsutil"];
-          };
-          # To be used in emergencies if the disk goes full
-          "local/reserved" = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "none";
-              refreservation = "1G";
-            };
-          };
-          "safe/persist" = {
-            type = "zfs_fs";
-            mountpoint = "/persist";
-            mountOptions = ["zfsutil"];
-          };
-          "safe/home" = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            mountOptions = ["zfsutil"];
-          };
-        };
-      };
-    };
-  };
-
-  # sadly disko can't handle this for us
-  fileSystems."/persist".neededForBoot = true;
-  fileSystems."/home".neededForBoot = true;
-
-  # Persistence (root dataset is rollback'ed to a blank snapshot)
-  networking.hostId = hostId;
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    zfs rollback -r rpool/local/root@blank
-  '';
 }

commit bcbfea3b6f1f127f0755e595ae41ba965e99d870

fix: impermanence being disabled in bacchus
Regression in c36bd9f

hosts/bacchus/default.nix
@@ -191,6 +191,7 @@
       zsh.enable = true;
     };
     impermanence = {
+      enable = true;
       directories = [
         "/etc/NetworkManager/system-connections"
         "/var/lib/bluetooth"