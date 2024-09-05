c08ed0eec1797d33f5f8e74265195c906ee81d0f
Diogo Correia <me@diogotc.com>
2024-09-05 19:36:25 +0200

    firefox: harden privacy settings

--- profiles/graphical/firefox.nix
+++ profiles/graphical/firefox.nix
@@ -5,9 +5,49 @@
     profiles.default = {
       id = 0;
       isDefault = true;
+      # Privacy settings from: https://github.com/arkenfox/user.js/blob/master/user.js
       settings = {
         # disable privacy invasive "private attribution" ad-tracking "feature"
         "dom.private-attribution.submission.enabled" = false;
+
+        # disable sponsorships on newtab page
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.default.sites" = "";
+
+        # disable recommendations in about:addons
+        "extensions.getAddons.showPane" = false;
+        "extensions.htmlaboutaddons.recommendations.enabled" = false;
+        "browser.discovery.enabled" = false;
+        "browser.shopping.experience2023.enabled" = false;
+
+        # disable telemetry
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "data:,";
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.coverage.opt-out" = true;
+        "toolkit.coverage.opt-out" = true;
+        "toolkit.coverage.endpoint.base" = "";
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+
+        # disable studies
+        "app.shield.optoutstudies.enabled" = false;
+        "app.normandy.enabled" = false;
+        "app.normandy.api_url" = "";
+
+        # disable (auto) crash reports
+        "breakpad.reportURL" = "";
+        "browser.tabs.crashReporting.sendReport" = false;
+        "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
       };
     };
   };
