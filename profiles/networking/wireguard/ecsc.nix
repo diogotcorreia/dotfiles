bbef52085d24fe53433d22014efc39ecedc496b4
Diogo Correia <me@diogotc.com>
2024-09-14 19:23:16 +0200

    wireguard-ecsc: update credentials for demo 3

--- profiles/networking/wireguard/ecsc.nix
+++ profiles/networking/wireguard/ecsc.nix
@@ -13,13 +13,13 @@
   networking.wg-quick.interfaces = {
     ecsc = {
       autostart = false;
-      address = ["10.81.31.10/32"];
+      address = ["10.81.30.10/32"];
       mtu = 1300;
       privateKeyFile = config.age.secrets.wireguardClientEcscPrivateKey.path;
 
       peers = [
         {
-          publicKey = "hNeM82sCEjqyBK45kRwaSKC9p2+P2YLaaQ+8knvqL3E=";
+          publicKey = "9giSIyMVFlxlz+TTfjHtlq1V/tS8u69bDtT26s82o3I=";
           endpoint = "vpn.ad.ecsc2024.it:51820";
           allowedIPs = ["10.254.0.0/24" "10.60.0.0/14" "10.64.0.0/14" "10.68.0.0/14" "10.80.0.0/16" "10.81.0.0/16" "10.10.0.0/16" "10.11.0.0/16"];
           persistentKeepalive = 5;
