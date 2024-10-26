5c7d5246896dcc515f33cc567812b9c25d6a7c21
Diogo Correia <me@diogotc.com>
2024-10-26 17:52:57 +0200

    feat(stalwart): add imap, catch-all, tls, autoconf, proper backups

--- profiles/services/stalwart-mail.nix
+++ profiles/services/stalwart-mail.nix
@@ -1,13 +1,23 @@
 # Stalwart mail server configuration
 {
+  config,
   inputs,
+  lib,
   pkgs,
   ...
 }: let
   dataDir = "/var/lib/stalwart-mail";
-  managementPort = 9988;
+  httpPort = 9988;
 
-  domain = "mail.acme-bnss.pt"; # TODO: change this to mail.diogotc.com after testing
+  domain = "acme-bnss.pt"; # TODO: change this to diogotc.com after testing
+  stalwartDomain = "mail.${domain}";
+  mailDomains = [
+    "acme-bnss.pt"
+  ];
+
+  credPath = "/run/credentials/stalwart-mail.service";
+
+  rocksdb = pkgs.unstable.rocksdb_8_11; # TODO: this should come from somewhere more permanent...
 in {
   # Use stalwart module from unstable
   # TODO: remove on nixos-24.11
@@ -22,10 +32,41 @@ in {
     enable = true;
     package = pkgs.unstable.stalwart-mail;
     settings = {
+      config.local-keys = [
+        "authentication.fallback-admin.*"
+        "certificate.*"
+        "cluster.node-id"
+        "directory.*"
+        "lookup.default.domain"
+        "lookup.default.hostname"
+        "report.analysis.*"
+        "server.*"
+        "!server.blocked-ip.*"
+        "session.rcpt.catch-all"
+        "storage.blob"
+        "storage.data"
+        "storage.directory"
+        "storage.fts"
+        "storage.lookup"
+        "store.*"
+        "tracer.*"
+      ];
+
       # Since system.stateVersion is lower than 24.11, we have to set some fields to use RocksDB instead of SQLite.
       store.db.type = "rocksdb";
       store.db.path = "${dataDir}/db";
       storage.blob = "db";
+
+      session.rcpt = {
+        # Enable catch-all addresses
+        catch-all = true;
+      };
+
+      report.analysis = {
+        # https://github.com/stalwartlabs/mail-server/discussions/877
+        addresses = ["dmarc-reports@*" "spf-reports@*"];
+        forward = false;
+      };
 
       server.listener = {
         smtp = {
@@ -36,41 +77,125 @@ in {
           bind = ["[::]:587"];
           protocol = "smtp";
         };
-        # submmissions = {
-        # bind = ["[::]:465"];
-        # protocol = "smtp";
-        # tls.implicit = true;
-        # };
-        management = {
-          bind = ["127.0.0.1:${toString managementPort}"];
+        submmissions = {
+          bind = ["[::]:465"];
+          protocol = "smtp";
+          tls.implicit = true;
+        };
+        imap = {
+          bind = ["[::]:143"];
+          protocol = "imap";
+        };
+        imaps = {
+          bind = ["[::]:993"];
+          protocol = "imap";
+          tls.implicit = true;
+        };
+        http = {
+          bind = ["[::]:${toString httpPort}"];
           protocol = "http";
+          url = "https://${stalwartDomain}";
+          use-x-forwarded = true;
+        };
+      };
+
+      certificate.default = {
+        cert = "%{file:${credPath}/cert.pem}%";
+        private-key = "%{file:${credPath}/key.pem}%";
+        default = true;
+      };
+      lookup = {
+        default = {
+          inherit domain;
+          hostname = stalwartDomain;
+        };
+      };
+
+      tracer = {
+        log = {
+          enable = true;
+          type = "log";
+          path = "%{env:LOGS_DIRECTORY}%";
+          prefix = "stalwart-mail.log";
+          level = "info";
         };
       };
 
       # Just for initial setup - comment immediately!
       # authentication.fallback-admin = {
-        # user = "admin";
-        # secret = "changemeasap";
+      # user = "admin";
+      # secret = "changemeasap";
       # };
     };
   };
 
   networking.firewall.allowedTCPPorts = [
     25 # SMTP
-    587 # SMTP Submission
+    143 # IMAP
     465 # SMTP Submission Secure
+    587 # SMTP Submission
+    993 # IMAP Secure
   ];
 
-  systemd.services.stalwart-mail.preStart = ''
-    mkdir -p ${dataDir}/db
-  '';
-
-  services.caddy.virtualHosts.${domain} = {
-    extraConfig = ''
-      reverse_proxy localhost:${toString managementPort}
+  systemd.services.stalwart-mail = {
+    wants = ["acme-${stalwartDomain}.service"];
+    after = ["acme-${stalwartDomain}.service"];
+    preStart = ''
+      mkdir -p ${dataDir}/db
     '';
+    serviceConfig = {
+      LogsDirectory = "stalwart-mail";
+      LoadCredential = [
+        "cert.pem:${config.security.acme.certs.${stalwartDomain}.directory}/cert.pem"
+        "key.pem:${config.security.acme.certs.${stalwartDomain}.directory}/key.pem"
+      ];
+    };
   };
 
+  services.caddy.virtualHosts =
+    {
+      ${stalwartDomain} = {
+        enableACME = true;
+        extraConfig = ''
+          reverse_proxy localhost:${toString httpPort}
+        '';
+      };
+    }
+    // lib.listToAttrs (
+      map (
+        d:
+          lib.nameValuePair
+          "mta-sts.${d}"
+          {
+            serverAliases = [
+              "autoconfig.${d}"
+              "autodiscovery.${d}"
+            ];
+            enableACME = true;
+            extraConfig = ''
+              @discovery path /mail/config-v1.1.xml /autodiscovery/autodiscovery.xml /.well-known/*
+              handle @discovery {
+                reverse_proxy localhost:${toString httpPort}
+              }
+              handle {
+                respond 404
+              }
+            '';
+          }
+      )
+      mailDomains
+    );
+
   modules.impermanence.directories = [dataDir];
-  modules.services.restic.paths = [dataDir];
+  modules.services.restic = {
+    backupPrepareCommand = ''
+      ${pkgs.coreutils}/bin/install -b -m 700 -d /tmp/stalwart-db-secondary /tmp/stalwart-db-backup
+      ${lib.getExe' rocksdb.tools "ldb"} --db=${dataDir}/db --secondary_path=/tmp/stalwart-db-secondary backup --backup_dir=/tmp/stalwart-db-backup
+    '';
+    backupCleanupCommand = ''
+      rm -rf /tmp/stalwart-db-secondary
+      rm -rf /tmp/stalwart-db-backup
+    '';
+    paths = ["/tmp/stalwart-db-backup"];
+  };
 }
