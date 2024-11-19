f61f810e945c9800b153c515f6fd095da9e20976
Diogo Correia <me@diogotc.com>
2024-11-19 11:53:22 +0100

    fix(apollo): use renamed hardware.graphics and force proprietary nvidia

--- hosts/apollo/nvidia.nix
+++ hosts/apollo/nvidia.nix
@@ -1,14 +1,17 @@
 # Enable (proprietary :/) NVIDIA drivers
 {config, ...}: {
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    enable32Bit = true;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
+    # GTX 1050Ti is not supported by open-source drivers
+    # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/19
+    open = false;
+
     modesetting.enable = true;
     nvidiaSettings = true;
 
