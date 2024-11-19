a4ad2768e9276454e9ace9925a2944a0c44031c0
Diogo Correia <me@diogotc.com>
2024-11-19 11:55:27 +0100

    fix(dmenu): missing math library during linking
    Also updated fuzzymatching patch to the latest version.

--- overlays/dmenu/default.nix
+++ overlays/dmenu/default.nix
@@ -3,11 +3,14 @@
   dmenu = prev.dmenu.overrideAttrs (oldAttrs: {
     patches = [
       (prev.fetchpatch {
-        url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.9.diff";
-        sha256 = "sha256-zfmsKfN791z6pyv+gA6trdfKvNnCCULazVtk1sibDgA=";
+        url = "https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-5.3.diff";
+        sha256 = "sha256-uPuuwgdH2v37eaefnbQ93ZTMvUBcl3LAjysfOEPD1Y8=";
       })
       # Based on https://tools.suckless.org/dmenu/patches/bar_height/
       ./0001-dmenu-bar-height.diff
     ];
+
+    # needed by fuzzymatch patch
+    NIX_LDFLAGS = "-lm";
   });
 }
