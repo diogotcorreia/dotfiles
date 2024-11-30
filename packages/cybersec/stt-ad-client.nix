8b2ac8d6a07d40c4a120a7e8963507193f35a3bb
Diogo Correia <me@diogotc.com>
2024-11-30 20:12:31 +0100

    chore: update stt-ad-client to latest commit

--- packages/cybersec/stt-ad-client.nix
+++ packages/cybersec/stt-ad-client.nix
@@ -8,10 +8,15 @@ python3Packages.buildPythonApplication {
   pname = "stt-ad-client";
   version = "0.0";
 
-  src = requireFile {
+  src = requireFile rec {
     name = "ad-client.tar.gz";
-    message = "The team captain should send you the ad-client tarball for the specific CTF";
-    hash = "sha256-mUzCCZvcb3HEYXE1+GLyipg5sY+am/9Mqmzdz44Hv5I=";
+    message = ''
+      The team captain should send you the ad-client tarball for the specific CTF.
+      The file can be imported with `nix-store --add-fixed sha256 ${name}`.
+      It is important that the file is named '${name}', otherwise it will not work.
+    '';
+    # get hash with `nix hash file ad-client.tar.gz`
+    hash = "sha256-Pl/z5y2975oe31tABkNXFMoDK01VkpBj9bD8rYqVTj8=";
   };
 
   propagatedBuildInputs = with python3Packages; [
