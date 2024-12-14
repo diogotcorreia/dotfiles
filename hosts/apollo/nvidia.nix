839482f4a21b91efcb4f17782fa6060d0481e9c2
Diogo Correia <me@diogotc.com>
2024-12-14 23:13:50 +0000

    fix(apollo,nvidia): screen freezing before console login
    See https://github.com/NixOS/nixpkgs/issues/328972

--- hosts/apollo/nvidia.nix
+++ hosts/apollo/nvidia.nix
@@ -12,9 +12,13 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/19
     open = false;
 
-    modesetting.enable = true;
     nvidiaSettings = true;
 
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  # Fixes not being able to boot into TTY
+  # https://github.com/NixOS/nixpkgs/issues/328972
+  hardware.nvidia.modesetting.enable = false;
+  boot.kernelParams = ["nvidia-drm.modeset=1"];
 }
