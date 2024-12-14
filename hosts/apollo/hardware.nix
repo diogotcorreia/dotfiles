e5b9e6fece5d3e9dbd8312d93b6e8eb1b1f16ef9
Diogo Correia <me@diogotc.com>
2024-12-14 23:14:55 +0000

    apollo: update hardware config from nixos-generate-config

--- hosts/apollo/hardware.nix
+++ hosts/apollo/hardware.nix
@@ -7,7 +7,7 @@
 }: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
