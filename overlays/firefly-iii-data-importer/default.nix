3134e568e9f356c14fccb5131f9617d115d3ef74
Diogo Correia <me@diogotc.com>
2024-12-14 23:48:27 +0000

    firefly-iii-data-importer: remove external ids fix patch
    The data importer has been upgraded and the patch is no longer needed
    (and it's failing to apply).

--- overlays/firefly-iii-data-importer/default.nix
+++ overlays/firefly-iii-data-importer/default.nix
@@ -2,12 +2,6 @@
 {...}: final: prev: {
   firefly-iii-data-importer = prev.firefly-iii-data-importer.overrideAttrs (oldAttrs: {
     patches = [
-      # Fix issue where too long external IDs in transactions break import
-      # https://github.com/firefly-iii/firefly-iii/issues/9347
-      (prev.fetchpatch {
-        url = "https://github.com/firefly-iii/data-importer/commit/abb351f268b0f91c52cc7076098d79f5661a8873.patch";
-        sha256 = "sha256-KTghYeQH7EQ5aDJvOsgdImuEg1H7qJ8vB9zagMbKiPI=";
-      })
       # Throw warning instead of error if transactions cannot be found
       ./0001-no-transactions-warning-instead-of-error.diff
     ];
