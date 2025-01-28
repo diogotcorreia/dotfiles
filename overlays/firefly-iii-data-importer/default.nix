58d9bc83bbb617bff4d78f79ec014743c3189a6f
Diogo Correia <me@diogotc.com>
2025-01-28 12:57:46 +0100

    feat(firefly-iii-data-importer): send file name in email report
    Send file name of JSON config in email report.
    This is helpful when the Nordigen/GoCardless EUA expires, otherwise it
    is not possible to know which account expired just from the email.

--- overlays/firefly-iii-data-importer/default.nix
+++ overlays/firefly-iii-data-importer/default.nix
@@ -4,6 +4,10 @@
     patches = [
       # Throw warning instead of error if transactions cannot be found
       ./0001-no-transactions-warning-instead-of-error.diff
+      # Send file name of JSON config in email report.
+      # This is helpful when the Nordigen/GoCardless EUA expires,
+      # otherwise it is not possible to know which account expired
+      ./0002-send-file-name-in-email.diff
     ];
   });
 }
