22c21f7c15244fc56c74e4e6ef37706e505fdfa4
Diogo Correia <me@diogotc.com>
2025-02-06 01:06:40 +0100

    fix(firefly-iii-data-importer): config being reset on start over button

--- overlays/firefly-iii-data-importer/default.nix
+++ overlays/firefly-iii-data-importer/default.nix
@@ -8,6 +8,9 @@
       # This is helpful when the Nordigen/GoCardless EUA expires,
       # otherwise it is not possible to know which account expired
       ./0002-send-file-name-in-email.diff
+      # Do not execute `artisan config:clear` when pressing the "Start Over"
+      # button, otherwise the env variables will be cleared.
+      ./0003-do-not-clear-config-when-starting-over.diff
     ];
   });
 }
