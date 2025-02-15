6448df92658ede179626d59f0bbab015c2ab447f
Diogo Correia <me@diogotc.com>
2025-02-15 16:49:34 +0000

    fix(discord): prevent ifd by generating config file in derivation
    See #15

--- profiles/graphical/discord.nix
+++ profiles/graphical/discord.nix
@@ -1,6 +1,7 @@
 # Discord configuration
 {
   config,
+  lib,
   pkgs,
   ...
 }: let
@@ -8,14 +9,15 @@
     url = "https://raw.githubusercontent.com/orblazer/discord-nordic/v4.10.22/uniform/nordic.theme.css";
     hash = "sha256-CyGBS5kzMCUieWSfzgwjYPlKHnKGRw0NY+AMBrV2aao=";
   };
-in {
-  hm.home.packages = with pkgs; [
-    # Discord
-    discord-openasar
-  ];
+  extraCss = ''
+    .theme-dark {
+      --background-primary: var(--primary-630);
+      --background-secondary: var(--primary-600);
+      --background-tertiary: var(--primary-700);
+    }
+  '';
 
-  # Discord configuration
-  hm.xdg.configFile."discord/settings-override.json".text = builtins.toJSON {
+  settings = {
     openasar = {
       setup = true;
       cmdPreset =
@@ -23,19 +25,28 @@ in {
         then "battery"
         else "perf";
       quickstart = true;
-      css = ''
-        ${builtins.readFile discordThemeFile}
-
-        .theme-dark {
-          --background-primary: var(--primary-630);
-          --background-secondary: var(--primary-600);
-          --background-tertiary: var(--primary-700);
-        }
-      '';
     };
     DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
     SKIP_HOST_UPDATE = true;
     MINIMIZE_TO_TRAY = false;
     OPEN_ON_STARTUP = false;
   };
+
+  settingsFile =
+    pkgs.runCommand "settings-override.json" {
+      nativeBuildInputs = with pkgs; [jq];
+    } ''
+      jq --argjson cfg ${lib.escapeShellArg (builtins.toJSON settings)} \
+        --arg css "$(<${discordThemeFile})" \
+        --arg extraCss ${lib.escapeShellArg extraCss} \
+        '$cfg * (.openasar.css = $css + "\n" + $extraCss)' -n > $out
+    '';
+in {
+  hm.home.packages = with pkgs; [
+    # Discord
+    discord-openasar
+  ];
+
+  # Discord configuration
+  hm.xdg.configFile."discord/settings-override.json".source = settingsFile;
 }
