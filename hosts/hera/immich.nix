fe00de8b93620960b25a8500eef7688fc94a039e
Diogo Correia <me@diogotc.com>
2025-04-13 01:14:58 +0200

    fix(immich): allow big uploads
    Max body size was 10MB, so photos over 10MB could not be uploaded.
    
    Related to d75c3f107a2074a1d87e0fbce934851a5a270abe

--- hosts/hera/immich.nix
+++ hosts/hera/immich.nix
@@ -116,6 +116,13 @@ in {
     ${domain} = {
       enableACME = true;
       locations."/".proxyPass = "http://[::1]:${toString port}";
+      # https://immich.app/docs/administration/reverse-proxy#nginx-example-config
+      extraConfig = ''
+        client_max_body_size 50000M;
+        proxy_read_timeout 600s;
+        proxy_send_timeout 600s;
+        send_timeout 600s;
+      '';
     };
   };
 
