be0cf84f08f6430e3a01814473e043521ba1a562
Diogo Correia <me@diogotc.com>
2025-04-15 11:09:54 +0200

    firefly-iii-auto-importer: change cron schedule to 09:13
    Seems like the problem is that the schedule is close to the hour mark,
    which probably sees a lot of traffic. Changing it to a random time
    should help.
    
    See 7120a6549436d87db274955b36165fd23eb6813f

--- hosts/hera/firefly-iii.nix
+++ hosts/hera/firefly-iii.nix
@@ -9,7 +9,7 @@
   domainApp = "firefly3.hera.diogotc.com";
   domainDataImporter = "firefly3-csv.hera.diogotc.com";
 
-  cronAutoDataImporter = "10:00";
+  cronAutoDataImporter = "09:13"; # avoid o'clock because it results in errors at banks
   configPathAutoDataImporter = "/persist/firefly-auto-import-configs";
 
   dbUser = config.services.firefly-iii.user;
