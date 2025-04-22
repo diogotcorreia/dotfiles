357b2e3df7bbb39e9c48896100e419f43ae1a3a1
Diogo Correia <me@diogotc.com>
2025-04-22 12:28:55 +0200

    security: hotfix for CVE-2025-32438
    The patch for 24.11 is still not available in the nixos-24.11, so this
    is a temporary hotfix until that lands.
    
    See https://discourse.nixos.org/t/security-advisory-local-privilege-escalation-in-make-initrd-ng-cve-2025-32438/63315

--- profiles/meta/common.nix
+++ profiles/meta/common.nix
@@ -1,6 +1,7 @@
 # Configuration that should be applied to all systems
 {profiles, ...}: {
   imports = with profiles; [
+    security.hotfix-cve-2025-32438
     services.ssh
     shell.programs.essential
   ];
