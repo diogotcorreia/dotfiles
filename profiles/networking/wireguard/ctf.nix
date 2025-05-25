257268ce2055f2ce424accb8a2b9e202cf84e5d9
Diogo Correia <me@diogotc.com>
2025-05-26 01:48:50 +0200

    wireguard: add teameurope config

--- profiles/networking/wireguard/ctf.nix
+++ profiles/networking/wireguard/ctf.nix
@@ -5,6 +5,7 @@
   ...
 }: {
   age.secrets = {
+    # pub key: NA8Z1i24Whrp8L+wScHjG1C63H8jhGle67wLX8hdI10=
     wireguardClientHeroisDoMarPrivateKey = {
       file = secrets.heroisDoMarWireguardPrivateKey;
     };
@@ -22,6 +23,21 @@
           publicKey = "oLcvAzvoij2jIgxGEPhOkGyeRSNzBJsFR4ljyB76gDM=";
           endpoint = "vpn.stt.rnl.pt:51820";
           allowedIPs = ["10.69.0.0/24" "10.32.0.0/15"];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+    teameurope = {
+      autostart = false;
+      address = ["10.128.24.112/32"];
+      mtu = 1320;
+      privateKeyFile = config.age.secrets.wireguardClientHeroisDoMarPrivateKey.path;
+
+      peers = [
+        {
+          publicKey = "3wf/CxDlNXO/x6yE3Tud31QS+fbQNG6s3UwW+TjL9WA=";
+          endpoint = "teameurope.duckdns.org:42434";
+          allowedIPs = ["10.128.16.1/20"];
           persistentKeepalive = 25;
         }
       ];
