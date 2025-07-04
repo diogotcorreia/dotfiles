6e169fd9bd5f844b3ffef5875486180ba9744d96
Diogo Correia <me@diogotc.com>
2025-07-04 12:37:09 +0100

    fix(calibre-web): build error
    See https://github.com/NixOS/nixpkgs/pull/422382

---
+++ overlays/calibre-web.nix
@@ -0,0 +1,7 @@
+# TODO: remove when https://github.com/NixOS/nixpkgs/pull/422382
+# reaches nixos-25.05 channel
+{...}: (_: prev: {
+  calibre-web = prev.calibre-web.overridePythonAttrs (oldAttrs: {
+    pythonRelaxDeps = (oldAttrs.pythonRelaxDeps or []) ++ ["tornado"];
+  });
+})
