b5b02838ef634cb0c0d657f66552f9d250d432be
Diogo Correia <me@diogotc.com>
2025-07-15 16:21:58 +0100

    refactor: use nix-index-database's renamed homeModules
    Upstream has renamed `hmModules` to `homeModules`. Using the prior now
    shows an evaluation warning.

--- profiles/shell/nix-index.nix
+++ profiles/shell/nix-index.nix
@@ -1,7 +1,7 @@
 # Setup nix-index with pre-built database
 {inputs, ...}: {
   home-manager.sharedModules = [
-    inputs.nix-index-database.hmModules.nix-index
+    inputs.nix-index-database.homeModules.nix-index
   ];
 
   hm.programs.nix-index.enable = true;
