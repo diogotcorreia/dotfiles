5df4297642e558cfdef47b72ebd187c5f245dd1e
Diogo Correia <me@diogotc.com>
2025-07-15 20:56:47 +0100

    zellij: add script to rename all tabs with their index

--- profiles/shell/zellij.nix
+++ profiles/shell/zellij.nix
@@ -1,5 +1,10 @@
 # Terminal multiplexer
-{...}: {
+{
+  config,
+  pkgs,
+  user,
+  ...
+}: {
   hm.programs.zellij = {
     enable = true;
 
@@ -10,4 +15,41 @@
       session_serialization = false;
     };
   };
+
+  hm.home.packages = [
+    # Script to rename all zellij tabs according to their position
+    # https://github.com/zellij-org/zellij/issues/3709#issuecomment-2766730939
+    (pkgs.writeShellApplication {
+      name = "zr";
+
+      runtimeInputs = [
+        config.home-manager.users.${user}.programs.zellij.package
+      ];
+      text = ''
+        if [[ -z "$ZELLIJ" ]]; then
+          echo "Not inside a Zellij session"
+          exit 1
+        fi
+
+        zellij action rename-tab "__CURRENT__"
+        TABS=$(zellij action query-tab-names)
+
+        zellij action go-to-tab 1
+
+        i=1
+        CURRENT_INDEX=0
+
+        while IFS= read -r TAB; do
+          if [[ "$TAB" == "__CURRENT__" ]]; then
+            CURRENT_INDEX=$i
+          fi
+          zellij action rename-tab "Tab #$i"
+          i=$((i + 1))
+          zellij action go-to-next-tab
+        done <<< "$TABS"
+
+        zellij action go-to-tab $CURRENT_INDEX
+      '';
+    })
+  ];
 }
