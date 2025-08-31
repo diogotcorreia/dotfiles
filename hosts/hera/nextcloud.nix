1192e774a27bfe1b3a051800c3c633f25bf4c654
Diogo Correia <me@diogotc.com>
2025-08-31 02:12:55 +0100

    fix(nextcloud): patch cospend to fix error in dashboard activity
    See https://github.com/julien-nc/cospend-nc/pull/352

--- hosts/hera/nextcloud.nix
+++ hosts/hera/nextcloud.nix
@@ -14,6 +14,8 @@ let
 
   dbUsername = "nextcloud";
   dbDatabaseName = "nextcloud";
+
+  ncApps = config.services.nextcloud.package.packages.apps;
 in
 {
   age.secrets.nextcloudSecrets = {
@@ -79,12 +81,12 @@ in
 
     appstoreEnable = false;
     extraAppsEnable = true;
-    extraApps = with config.services.nextcloud.package.packages.apps; {
+    extraApps = with ncApps; {
       inherit
         calendar
         contacts
         cookbook
-        cospend
+        # cospend (see below)
         deck
         gpoddersync # podcasts sync service
         # nextpod (see below)
@@ -93,6 +95,15 @@ in
         tasks
         twofactor_webauthn
         ;
+
+      cospend = ncApps.cospend.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/julien-nc/cospend-nc/commit/2682e13e51c34f82dd8d8929973a9413c635d73c.patch";
+            hash = "sha256-//rL7saVY+ZQeSQUgoZk+W/2M8glZYSL0iDHgrnlBgc=";
+          })
+        ];
+      });
 
       # https://apps.nextcloud.com/apps/nextpod
       nextpod = pkgs.fetchNextcloudApp rec {
