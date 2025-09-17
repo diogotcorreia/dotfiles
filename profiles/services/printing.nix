db20180123c914e9e6cfe581e3d96b9f285b5f6b
Diogo Correia <me@diogotc.com>
2025-09-17 13:31:02 +0100

    profiles/printing: install gutenprint driver

--- profiles/services/printing.nix
+++ profiles/services/printing.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [
+    gutenprint
+  ];
   services.avahi = {
     enable = true;
     nssmdns4 = true;
