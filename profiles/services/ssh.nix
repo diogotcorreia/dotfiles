151a27cd81361029b064141d8008cb082a5c8ad8
Diogo Correia <me@diogotc.com>
2025-09-17 16:21:02 +0100

    profiles/ssh: replace phone ssh key

--- profiles/services/ssh.nix
+++ profiles/services/ssh.nix
@@ -4,7 +4,7 @@ let
   sshKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYiuCHjX9Dmq69WoAn7EfgovnFLv0VhjL7BSTYQcFa7 dtc@apollo"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlaWu32ANU+sWFcwKrPlqD/oW3lC3/hrA1Z3+ubuh5A dtc@bacchus"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICmAw3MrBc3MERcNBkerJwfh9fmfD1OCeYnLVJVxs2Rs dtc@xiaomi11tpro"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJG5pN82I+kQMlD49a7aUum+ms5wUXZoqW03BBjULONA dtc@bluejay"
   ];
 in
 {
