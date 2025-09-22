91ce7fef856bcb598a92b30bf13732a12416ab41
Diogo Correia <me@diogotc.com>
2025-09-22 16:14:47 +0200

    alt-urls-discord-bot: 0.2.1 -> 0.2.2

--- packages/alt-urls-discord-bot.nix
+++ packages/alt-urls-discord-bot.nix
@@ -7,17 +7,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "alt-urls-discord-bot";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "diogotcorreia";
     repo = "alt-urls-discord-bot";
     rev = "v${version}";
-    hash = "sha256-4TCELsFRY5EuewQq+jGKAFput7GpXhZQkwCIs/WO0oY=";
+    hash = "sha256-bqpbAlo9vR0IxKFnRLMliurbPT0aAdZ+2UYWdYt7ZD4=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-MvvYUHfwTcwzY2Qpc3wU/RcA15uavZ1vDS6k1KxqcPc=";
+  cargoHash = "sha256-gKp1sp9uLpMt9AiZcmY3kk1l3D2p1NXLrJgJk8T73h0=";
 
   meta = with lib; {
     description = "Discord bot that provides alternative links to various social media services ";
