9c67070b98206acc52945c75a365537de7417001
Diogo Correia <me@diogotc.com>
2025-10-11 17:18:32 +0200

    caido: add ozone platform hint for wayland

--- profiles/graphical/caido/default.nix
+++ profiles/graphical/caido/default.nix
@@ -74,7 +74,8 @@ in
           install -m 444 -D ${appImageContents}/caido.png \
             $out/share/icons/hicolor/512x512/apps/caido.png
           wrapProgram $out/bin/caido \
-            --set WEBKIT_DISABLE_COMPOSITING_MODE 1
+            --set WEBKIT_DISABLE_COMPOSITING_MODE 1 \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
         '';
       };
 
