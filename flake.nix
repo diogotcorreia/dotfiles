29f4ea4bdfe25b1ef2ea622880b7470de7697fe1
Diogo Correia <me@diogotc.com>
2025-10-16 16:23:26 +0200

    pwndbg: 2025.04.18 -> 2025.10.10

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     # allow it to be cached and re-exported for my ctf flake
     # https://github.com/NixOS/nixpkgs/pull/380600
     pwndbg = {
-      url = "github:pwndbg/pwndbg?ref=2025.04.18";
+      url = "github:pwndbg/pwndbg?ref=2025.10.10";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lidl-to-grocy = {
