9d20ae1ad327d82d240a6e2c73da7805647a3f73
Diogo Correia <me@diogotc.com>
2025-11-05 19:27:48 +0100

    nextcloud: 31 -> 32

--- hosts/hera/nextcloud.nix
+++ hosts/hera/nextcloud.nix
@@ -24,7 +24,7 @@ in
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud32;
     hostName = domain;
     https = true;
     database.createLocally = true; # automatically uses pgsql through sockets
