3e376106c609bd407e04cf77b1e3ad8546284ba2
Diogo Correia <me@diogotc.com>
2025-11-11 12:11:04 +0100

    geoclue: set location.provider
    Removed forced beacondb API URL since it is now the default in nixpkgs.

--- profiles/misc/geoclue.nix
+++ profiles/misc/geoclue.nix
@@ -1,7 +1,5 @@
 # Geoclue2 configuration
 { ... }:
 {
-  # Use https://beacondb.net/, since Mozilla's service has been shutdown
-  # See https://github.com/mozilla/ichnaea/issues/2065
-  services.geoclue2.geoProviderUrl = "https://beacondb.net/v1/geolocate";
+  location.provider = "geoclue2";
 }
