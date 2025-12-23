c679d3c354a142c8ee561357f8768af235ce8b84
Diogo Correia <me@diogotc.com>
2025-12-23 19:23:43 +0000

    fuzzel: add styles

--- profiles/graphical/fuzzel.nix
+++ profiles/graphical/fuzzel.nix
@@ -1,5 +1,8 @@
 # Application launcher
-{ ... }:
+{ lib, ... }:
+let
+  toFuzzelColor = c: "${lib.substring 1 (-1) c}ff";
+in
 {
   hm.programs.fuzzel = {
     enable = true;
@@ -7,6 +10,22 @@
       main = {
         list-executables-in-path = true;
       };
+
+      colors = with lib.my.colors; {
+        background = toFuzzelColor black;
+        text = toFuzzelColor lightwhite;
+        prompt = toFuzzelColor white;
+        placeholder = toFuzzelColor darkwhite;
+        input = toFuzzelColor lightwhite;
+        match = toFuzzelColor red;
+        selection = toFuzzelColor grey;
+        selection-text = toFuzzelColor lightwhite;
+        selection-match = toFuzzelColor red;
+        counter = toFuzzelColor darkwhite;
+        border = toFuzzelColor lightblue;
+      };
+
+      border.width = 2;
     };
   };
 }
