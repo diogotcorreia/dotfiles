52249c4d9bcc01ebd4d5b1162fe14cefd748a80d
Diogo Correia <me@diogotc.com>
2025-12-23 02:26:40 +0000

    swaylock: style with color scheme

--- profiles/graphical/swaylock.nix
+++ profiles/graphical/swaylock.nix
@@ -1,16 +1,50 @@
-{ ... }:
+{ lib, ... }:
 {
   security.pam.services.swaylock = { };
   hm.programs.swaylock = {
     enable = true;
     # TODO: play around with settings
     settings = {
-      color = "808080";
       font-size = 24;
       indicator-idle-visible = false;
       indicator-radius = 100;
-      line-color = "ffffff";
       show-failed-attempts = true;
-    };
+      ignore-empty-password = true;
+    }
+    // (with lib.my.colors; {
+      # colors
+      color = black;
+      bs-hl-color = orange;
+      key-hl-color = teal;
+      caps-lock-bs-hl-color = orange;
+      caps-lock-key-hl-color = teal;
+      separator-color = black;
+      layout-bg-color = "${black}c0";
+      layout-text-color = lightwhite;
+
+      inside-color = "${grey}c0";
+      inside-clear-color = "${yellow}c0";
+      inside-caps-lock-color = "${black}c0";
+      inside-ver-color = "${lightblue}c0";
+      inside-wrong-color = "${red}c0";
+
+      line-color = black;
+      line-clear-color = black;
+      line-caps-lock-color = black;
+      line-ver-color = black;
+      line-wrong-color = black;
+
+      ring-color = green;
+      ring-clear-color = yellow;
+      ring-caps-lock-color = yellow;
+      ring-ver-color = darkblue;
+      ring-wrong-color = pink;
+
+      text-color = orange;
+      text-clear-color = black;
+      text-caps-lock-color = orange;
+      text-ver-color = black;
+      text-wrong-color = black;
+    });
   };
 }
