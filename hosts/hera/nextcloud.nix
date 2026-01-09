3dc942a3d43d098b6a43bb48e179111b67e519c0
Diogo Correia <me@diogotc.com>
2026-01-09 00:08:48 +0000

    nextcloud: enable opcache invalidation
    Nextcloud has been having a weird bug where it stops responding to
    requests and its CPU usage jumps to 100%.
    This is an attempt at debugging the problem.

--- hosts/hera/nextcloud.nix
+++ hosts/hera/nextcloud.nix
@@ -50,7 +50,8 @@ in
     };
     phpOptions = {
       "opcache.interned_strings_buffer" = "16";
-      "opcache.validate_timestamps" = "0"; # disable opcache invalidation; flushed on nixos activation script
+      "opcache.validate_timestamps" = "1";
+      "opcache.revalidate_freq" = "60";
       "opcache.jit" = "1255";
       "opcache.jit_buffer_size" = "128M";
     };
@@ -264,8 +265,6 @@ in
   # Nextcloud stops working for a brief moment.
   # This happens because the secrets handled by agenix change their path,
   # and PHP does not follow the new destination of the symlink until the cache expires.
-  # Additionally, we are now disabling OPCache's invalidation, given that files do not change
-  # without a NixOS activation.
   # This reloads php-fpm after agenix has updated secrets, so that it clears the cache.
   system.activationScripts.nextcloud-reload = {
     text = ''
