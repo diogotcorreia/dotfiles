34b60987ddf8ae701e324f64e377ca1aa3eae44f
Diogo Correia <me@diogotc.com>
2026-01-19 16:10:51 +0100

    modules/personal: use dix instead of nvd for activation script diff

--- modules/personal.nix
+++ modules/personal.nix
@@ -148,7 +148,7 @@ in
     system.activationScripts.diff = {
       supportsDryActivation = true;
       text = ''
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+        ${lib.getExe pkgs.dix} /run/current-system "$systemConfig"
       '';
     };
   };
