051baeaa7ef29d6e3abed0c8c956c659a6c89ee1
Diogo Correia <me@diogotc.com>
2026-01-20 02:30:07 +0100

    dtc-labs: 0-unstable-2025-11-10 -> 0-unstable-2026-01-20

--- packages/dtc-labs.nix
+++ packages/dtc-labs.nix
@@ -6,29 +6,27 @@
   makeWrapper,
   nodejs,
   stdenv,
-  yarnBuildHook,
   yarnConfigHook,
   yarnInstallHook,
   ...
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "dtc-labs";
-  version = "0-unstable-2025-11-10";
+  version = "0-unstable-2026-01-20";
   src = fetchFromGitHub {
     owner = "diogotcorreia";
     repo = "dtc-labs";
-    rev = "065cde59a779a08028815e820e25a1e8ac1a1eb0";
-    hash = "sha256-rRE+xRFuV2vw01AAuLOeVVMlBTI5yfZkuE9Jn+I4mow=";
+    rev = "bc7d557e772d256ef3036ff96132fe6181bd2a14";
+    hash = "sha256-w8DWAbdlp23mJofVK6ynBsGOFE/o2W86poq3ZtLxGPY=";
   };
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = finalAttrs.src + "/yarn.lock";
-    sha256 = "sha256-A3HDyDqJh3RBSgABaC4sl0RHWmcujLaU6i7ML4RAPdc=";
+    sha256 = "sha256-cytlWPIa6sWsJxvu+iOF12spdGEehMuSCR0EFCRscFI=";
   };
 
   nativeBuildInputs = [
     yarnConfigHook
-    yarnBuildHook
     yarnInstallHook
     makeWrapper
     nodejs
@@ -37,20 +35,20 @@ stdenv.mkDerivation (finalAttrs: {
   postInstall = ''
     OUT_JS_DIR="$out/lib/node_modules/${finalAttrs.pname}"
 
-    cp -r dist "$OUT_JS_DIR"
+    # yarnInstallHook copies everything over already
 
     # generate binary
     makeWrapper '${lib.getExe nodejs}' "$out/bin/${finalAttrs.pname}" \
-      --add-flags "$OUT_JS_DIR/dist/index.js"
+      --add-flags "$OUT_JS_DIR/src/index.js"
 
     # delete unnecessary files
-    rm -r "$OUT_JS_DIR"/{src,package.json,README.md,.babelrc,.prettierrc}
+    rm -r "$OUT_JS_DIR"/{package.json,README.md,.prettierrc,LICENSE,.husky,renovate.json,shell.nix}
   '';
 
   meta = with lib; {
     description = "Experimental code snippets for DTC";
     homepage = "https://github.com/diogotcorreia/dtc-labs";
-    license = licenses.free;
+    license = licenses.gpl3Plus;
     mainProgram = "dtc-labs";
     platforms = platforms.all;
   };
