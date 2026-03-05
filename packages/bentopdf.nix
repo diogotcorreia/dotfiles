523300ab2e85faabe7ae640fb81dda3f342d1591
Diogo Correia <me@diogotc.com>
2026-03-06 00:44:02 +0100

    bentopdf: 2.2.1 -> 2.4.0

--- packages/bentopdf.nix
+++ packages/bentopdf.nix
@@ -17,16 +17,16 @@ let
   };
 in
 buildNpmPackage' (finalAttrs: {
-  version = "2.2.1";
+  version = "2.4.0";
   pname = "bentopdf";
 
   src = fetchFromGitHub {
     owner = "alam00000";
     repo = "bentopdf";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-nrUdBLbVOqP2v719nHaoCuyBYVxhONeEC4il2dwikPU=";
+    hash = "sha256-nzrVLs0x/mRCEtTOXXuhbWd2BJAOI262BcZ8r4eJdnw=";
   };
-  npmDepsHash = "sha256-3ydO5MnhKETSDpW+ntd9lJIx9erzfvBijNMFkDJKrqQ=";
+  npmDepsHash = "sha256-1xWkzAzZXd9f4sY9xFHLfj482ZCs2oKFJ79C+Iy7RFY=";
 
   npmBuildScript = "build";
   npmBuildFlags = [
