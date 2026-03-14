2a87a542ade14bbb64e1ec05935336c2f6d05425
Diogo Correia <me@diogotc.com>
2026-03-14 19:44:55 +0100

    modules/postgresql: add ensureUsers.ensureDBOwnershipOf option

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -5,12 +5,40 @@
   ...
 }:
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkOption types;
 
   resticCfg = config.modules.services.restic;
   postgresqlCfg = config.services.postgresql;
+
+  psql = lib.getExe' postgresqlCfg.package "psql";
+  ownershipClauses = lib.concatMap (
+    user:
+    map (
+      db: ''${psql} -tAc 'ALTER DATABASE "${db}" OWNER TO "${user.name}";' ''
+    ) user.ensureDBOwnershipOf
+  ) postgresqlCfg.ensureUsers;
 in
 {
+  options.services.postgresql = {
+    ensureUsers = mkOption {
+      type = types.listOf (
+        types.submodule ({
+          options = {
+            ensureDBOwnershipOf = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = ''
+                Grants the user ownership to the given databases.
+                This databases must be defined manually in
+                [](#opt-services.postgresql.ensureDatabases).
+              '';
+            };
+          };
+        })
+      );
+    };
+  };
+
   config = mkIf postgresqlCfg.enable {
     # Tweak PostgreSQL performance for SSDs
     # https://pgtune.leopard.in.ua/?dbVersion=18&osType=linux&dbType=web&cpuNum=&totalMemory=4&totalMemoryUnit=GB&connectionNum=&hdType=ssd
@@ -40,11 +68,11 @@ in
         name = "restic";
       }
     ];
-    systemd.services.postgresql-setup.serviceConfig.ExecStartPost = mkIf resticCfg.enable [
-      ''
-        ${lib.getExe' postgresqlCfg.package "psql"} -c "GRANT pg_read_all_data TO restic;"
-      ''
-    ];
+    systemd.services.postgresql-setup.serviceConfig.ExecStartPost =
+      lib.optionals resticCfg.enable [
+        "${psql} -c 'GRANT pg_read_all_data TO restic;'"
+      ]
+      ++ ownershipClauses;
 
     # Persist databases when using tmpfs
     modules.impermanence.directories = [ "/var/lib/postgresql" ];
