6d21000459900d97e9d62a00044d873439b14367
Diogo Correia <me@diogotc.com>
2026-04-16 19:08:25 +0200

    refactor: move formatter script to its own package

--- flake.nix
+++ flake.nix
@@ -160,34 +160,7 @@
       };
 
       formatter = {
-        # https://github.com/NixOS/nix/pull/11438#issuecomment-2343378813
-        x86_64-linux = pkgs.writeShellScriptBin "formatter" ''
-          set -e
-
-          OPTIND=1
-
-          DEADNIX_ARGS=()
-          NIXFMT_ARGS=()
-          while getopts "c" opt; do
-            case "$opt" in
-              c)
-                DEADNIX_ARGS+=("--fail")
-                NIXFMT_ARGS+=("--ci")
-                ;;
-            esac
-          done
-          shift $((OPTIND-1))
-          [ "''${1:-}" = "--" ] && shift
-
-          # If no arguments are passed, default to formatting the whole project
-          if [[ $# = 0 ]]; then
-            prj_root=$(git rev-parse --show-toplevel 2>/dev/null || echo .)
-            set -- "$prj_root"
-          fi
-
-          "${lib.getExe pkgs.deadnix}" --hidden --edit "''${DEADNIX_ARGS[@]}" -- "$@"
-          "${lib.getExe pkgs.nixfmt-tree}" "''${NIXFMT_ARGS[@]}" -- "$@"
-        '';
+        x86_64-linux = pkgs.my.formatter;
       };
     };
 }
