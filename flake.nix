06354eab613b0cd40273ad94231a1572bab440a7
Diogo Correia <me@diogotc.com>
2026-05-30 11:51:18 +0200

    chore: point nixpkgs flake input to nixos-26.05
    Part of #150

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Diogo Correia's Nix(OS) configuration for PCs and servers";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05-small"; # TODO: move to nixos-26.05 when available
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     impermanence = {
       url = "github:nix-community/impermanence";
