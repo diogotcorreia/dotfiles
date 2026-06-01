diogotcorreia/dotfiles

commit 5dd8c4fd2ed8fd18f5a4b8e8843fba02b062c981

chore: update to nixos 26.05
This is the first commit updating to NixOS 26.05. This commit fixes all
the evaluation errors and warnings (which are treated as errors in CI).

Further changes and migrations will come in later commits.

packages/pocket-grimoire.nix
@@ -34,8 +34,7 @@ php.buildComposerProject2 (finalAttrs: {
   };
 
   composerStrictValidation = false;
-  composerNoPlugins = false;
-  vendorHash = "sha256-DeUakUv1y2rvg8kIqQ2MJGEn9uxA+SC7YmpUMhnM9kA=";
+  vendorHash = "sha256-vOOI6dthIRnK9ISjVOSxOxudk0Dg5zuBMYH7Vzbf62E=";
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = finalAttrs.src + "/yarn.lock";

flake.nix
@@ -2,7 +2,7 @@
   description = "Diogo Correia's Nix(OS) configuration for PCs and servers";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05-small"; # TODO: move to nixos-26.05 when available
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     impermanence = {
       url = "github:nix-community/impermanence";
@@ -10,14 +10,12 @@
       inputs.home-manager.follows = "home";
     };
     home = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master"; # TODO: move to release-26.05 when available
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim/main"; # TODO: move to nixos-26.05 when available
       inputs.nixpkgs.follows = "nixpkgs";
-      # we don't need to build docs, so we can get rid of it
-      inputs.nuschtosSearch.follows = "";
     };
     agenix = {
       url = "github:ryantm/agenix/main";
@@ -61,7 +59,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ist-discord-bot = {
-      url = "github:ist-bot-team/ist-discord-bot?ref=v3.0.5";
+      url = "github:ist-bot-team/ist-discord-bot?ref=v3.0.7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs-build-failure-notifier = {

hosts/bacchus/default.nix
@@ -42,6 +42,7 @@
   # ZFS
   boot.supportedFilesystems = [ "zfs" ];
   boot.kernelParams = [ "nohibernate" ];
+  boot.zfs.forceImportRoot = false; # TODO 26.11: remove after changing stateVersion
   networking.hostId = "239be557";
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;

hosts/hera/device-aliases.nix
@@ -1,7 +1,7 @@
 # Nginx configuration for device domain aliases (i.e. access local devices from outside the network)
 { lib, ... }:
 let
-  inherit (lib) fold recursiveUpdate;
+  inherit (lib) foldr recursiveUpdate;
   defineAlias =
     domain: target:
     {
@@ -19,7 +19,7 @@ let
       };
     };
 
-  mergeAliases = listOfAttrsets: fold (attrset: acc: recursiveUpdate attrset acc) { } listOfAttrsets;
+  mergeAliases = listOfAttrsets: foldr (attrset: acc: recursiveUpdate attrset acc) { } listOfAttrsets;
 in
 mergeAliases [
   (defineAlias "apollo.diogotc.com" "http://192.168.1.2:8080" { })

hosts/hera/immich.nix
@@ -34,7 +34,6 @@ in
 
     enable = true;
     mediaLocation = photosLocation;
-    database.enableVectors = false;
 
     settings = {
       # disable built-in database backup; we already have our own

hosts/hera/jellyfin.nix
@@ -51,7 +51,7 @@ in
   };
 
   services.jellyfin.enable = true;
-  services.jellyseerr = {
+  services.seerr = {
     enable = true;
     port = portJellyseerr;
     # This might change, so pin it to make sure it doesn't break
@@ -121,8 +121,9 @@ in
     ];
     ensureUsers = [
       {
-        name = jellyseerrDb;
-        ensureDBOwnership = true;
+        # TODO: rename DB to match user
+        name = "seerr";
+        ensureDBOwnershipOf = [ jellyseerrDb ];
         ensureClauses.login = true;
       }
       {
@@ -143,7 +144,7 @@ in
       }
     ];
   };
-  systemd.services.jellyseerr = {
+  systemd.services.seerr = {
     wants = [ "postgresql.target" ];
     after = [ "postgresql.target" ];
     environment = {
@@ -283,7 +284,7 @@ in
     # also persist cache so we don't have to fetch metadata on every reboot
     config.services.jellyfin.cacheDir
 
-    (lib.my.toPrivateStateDirectory config.services.jellyseerr.configDir)
+    (lib.my.toPrivateStateDirectory config.services.seerr.configDir)
     config.services.radarr.dataDir
     config.services.sonarr.dataDir
     config.services.jackett.dataDir
@@ -292,7 +293,7 @@ in
 
   modules.services.restic.paths = [
     config.services.jellyfin.dataDir
-    (lib.my.toPrivateStateDirectory config.services.jellyseerr.configDir)
+    (lib.my.toPrivateStateDirectory config.services.seerr.configDir)
     config.services.radarr.dataDir
     config.services.sonarr.dataDir
     config.services.jackett.dataDir
@@ -302,8 +303,8 @@ in
     "${config.services.jellyfin.dataDir}/log"
     "${config.services.jellyfin.dataDir}/metadata/livetv"
     "${config.services.jellyfin.dataDir}/transcode"
-    "${lib.my.toPrivateStateDirectory config.services.jellyseerr.configDir}/cache"
-    "${lib.my.toPrivateStateDirectory config.services.jellyseerr.configDir}/logs"
+    "${lib.my.toPrivateStateDirectory config.services.seerr.configDir}/cache"
+    "${lib.my.toPrivateStateDirectory config.services.seerr.configDir}/logs"
     "${config.services.radarr.dataDir}/logs"
     "${config.services.sonarr.dataDir}/logs"
     "${config.services.jackett.dataDir}/log.txt"

hosts/hera/nextcloud.nix
@@ -36,7 +36,7 @@ in
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
     hostName = domain;
     https = true;
     database.createLocally = true; # automatically uses pgsql through sockets

modules/graphical/gtk.nix
@@ -28,6 +28,8 @@ in
         package = pkgs.adwaita-icon-theme;
         name = "Adwaita";
       };
+
+      gtk4.theme = null;
     };
   };
 }

commit 1aad50f28d3aca243776827892d15683bc48c94f

pocket-grimoire: re-enable plugins and fix build
Commit 5dd8c4fd2ed8fd18f5a4b8e8843fba02b062c981 broke the runtime of
pocket-grimoire as it stopped including the symfony runtime by disabling
plugins.

Plugins were disabled due to a build failure caused by symfony/flex
attempting to download files during installation, which seems to be
related to a cache miss caused by [1].

This commit disables the symfony/flex plugin, which prevents the build
failure but allows the symfony/runtime plugin to run.

[1]: https://github.com/NixOS/nixpkgs/pull/459254/changes#r3314016253

packages/pocket-grimoire.nix
@@ -1,4 +1,5 @@
 {
+  applyPatches,
   fetchFromGitHub,
   fetchYarnDeps,
   lib,
@@ -26,15 +27,22 @@ php.buildComposerProject2 (finalAttrs: {
   pname = "pocket-grimoire";
   version = "2026.05.07";
 
-  src = fetchFromGitHub {
-    owner = "diogotcorreia";
-    repo = "pocket-grimoire";
-    tag = finalAttrs.version;
-    hash = "sha256-A4UP0LQ93Tly8KwoPOGg5C2mCf6azeKhSMsiBLJj2W4=";
+  src = applyPatches {
+    src = fetchFromGitHub {
+      owner = "diogotcorreia";
+      repo = "pocket-grimoire";
+      tag = finalAttrs.version;
+      hash = "sha256-A4UP0LQ93Tly8KwoPOGg5C2mCf6azeKhSMsiBLJj2W4=";
+    };
+    postPatch = ''
+      substituteInPlace ./composer.json \
+        --replace-fail '"symfony/flex": true,' '"symfony/flex": false,'
+    '';
   };
 
   composerStrictValidation = false;
-  vendorHash = "sha256-vOOI6dthIRnK9ISjVOSxOxudk0Dg5zuBMYH7Vzbf62E=";
+  composerNoPlugins = false;
+  vendorHash = "sha256-UZQGCClrNNIlBghNRbkVH7+glFV45Ss5G7VndI+31G0=";
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = finalAttrs.src + "/yarn.lock";