a190bc723245ff6e8d8674ebfde63fde346cf0a7
Diogo Correia <me@diogotc.com>
2026-07-18 19:48:14 +0100

    nextcloud: update to nextcloud 34

--- hosts/hera/nextcloud.nix
+++ hosts/hera/nextcloud.nix
@@ -36,7 +36,7 @@ in
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud33;
+    package = pkgs.nextcloud34;
     hostName = domain;
     https = true;
     database.createLocally = true; # automatically uses pgsql through sockets
