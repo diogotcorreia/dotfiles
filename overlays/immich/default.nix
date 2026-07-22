29f755d164323d6137c17068bf4b6f5eee79e604
Diogo Correia <me@diogotc.com>
2026-07-22 22:22:41 +0100

    immich: fix upload failing when using iOS Safari

--- overlays/immich/default.nix
+++ overlays/immich/default.nix
@@ -12,6 +12,12 @@ _final: prev: {
         # Avoid downloading archives to RAM first
         # https://github.com/immich-app/immich/pull/30021
         ./0002-download-archive-html-forms-pr-30021.diff
+        # Fix iOS Safari upload input
+        # https://github.com/immich-app/immich/pull/29660
+        (prev.fetchpatch {
+          url = "https://github.com/immich-app/immich/commit/89d0a9d59f1b7ca2854ee76206ec0fa748867580.patch";
+          hash = "sha256-YWfKL10eK89KcXKhu7ftD/aP3+rDGGQRvWDHEpWqFxE=";
+        })
       ];
     };
   });
