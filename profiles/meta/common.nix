affdde63227a11f1f3ead4b986adeccb92cac21e
Diogo Correia <me@diogotc.com>
2026-08-06 18:39:26 +0100

    Revert "fix: hotfix for dirty frag vulnerability (dirtyfrag.io)"
    This reverts commit 0d638accdd368fd98a2c2206ccd50f3c43122ebf.

--- profiles/meta/common.nix
+++ profiles/meta/common.nix
@@ -10,11 +10,4 @@
 
   # Modern defaults
   networking.nftables.enable = true;
-
-  # Mitigation for dirtyfrag vulnerability
-  boot.extraModprobeConfig = ''
-    install esp4 /bin/false
-    install esp6 /bin/false
-    install rxrpc /bin/false
-  '';
 }
